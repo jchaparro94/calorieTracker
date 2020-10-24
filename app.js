db37d9a8895ea375afa8d446d678990b770de117
Jose <chaparro.jjr@gmail.com>
2020-10-24 16:09:38 -0700

    added meals will now appear in the DOM

--- app.js
+++ app.js
@@ -107,6 +107,23 @@ const UICtrl = (function () {
         calories: document.querySelector(UISelectors.itemCaloriesInput).value
       }
     },
+    addListItem: function (item) {
+      // Create li element 
+      const li = document.createElement('li');
+      // Add classes to the li 
+      li.className = 'collection-item';
+      // Add ID 
+      li.id = `item-${item.id}`;
+      // Add html 
+      li.innerHTML = `
+      <strong>${item.name}: </strong><em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i>
+          </a>
+      `;
+      // Insert item 
+      document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+    },
     getSelectors: function () {
       return UISelectors;
     }
@@ -143,6 +160,8 @@ const App = (function (ItemCtrl, UICtrl) {
     if (input.name !== '' && input.calories !== '') {
       // Add item 
       const newItem = ItemCtrl.addItem(input.name, input.calories);
+      // Add item to the UI list 
+      UICtrl.addListItem(newItem);
     }
 
 
